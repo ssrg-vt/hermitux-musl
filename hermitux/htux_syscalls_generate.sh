9af6963db6769a3a3baaea760942a877b2b69456
Pierre Olivier <polivier@vt.edu>
2019-08-22 10:33:08 -0400

    Update htux syscall generation script to include aarch64

--- hermitux/htux_syscalls_generate.sh
+++ hermitux/htux_syscalls_generate.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 HERMITUX_BASE=$HOME/Desktop/hermitux
-HERMITUX=$HERMITUX_BASE/hermitux-kernel/prefix/x86_64-hermit/extra/tests/hermitux
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 <arch>"
+    echo "  where arch is 'x86-64' or 'aarch64'"
+    exit
+fi
 
 echo "static void unsupported(int id);"
 echo
 
 echo "static void *syscalls[335] = {"
 
-syscalls=`cat syscalls_data | cut -d "," -f 3 | awk 'BEGIN { ORS = " " } { print }'`
+if [ "$1" == "aarch64" ]; then
+    syscalls=`cat syscalls_data.aarch64 | cut -d "," -f 3 | awk 'BEGIN { ORS = " " } { print }'`
+    HERMITUX=$HERMITUX_BASE/hermitux-kernel/prefix/aarch64-hermit/extra/tests/hermitux
+else
+    syscalls=`cat syscalls_data.x86-64 | cut -d "," -f 3 | awk 'BEGIN { ORS = " " } { print }'`
+    HERMITUX=$HERMITUX_BASE/hermitux-kernel/prefix/x86_64-hermit/extra/tests/hermitux
+fi
 
 id=0
 for f in $syscalls; do
